19f91268f699685ecc21917546297faba5afe836
CHERRADI NABIL Mouhssine <46906074+cnabildev@users.noreply.github.com>
2024-10-10 22:33:05 +0100

    complete challenge remove Element

--- RemoveElement/src/Solution.java
+++ RemoveElement/src/Solution.java
@@ -1,5 +1,29 @@
 class Solution {
+    // create main function
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int result = solution.removeElement(nums, val);
+        System.out.println(result);
+    }
+
     public int removeElement(int[] nums, int val) {
-        return 0;
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                // print  nums[k] = nums[i];
+                System.out.println("nums[" + k + "]" + "=" + "nums[" + i + "]");
+                k++;
+            }
+        }
+        // Print the array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        return k;
+
     }
 }
